7e88b61780652fc5341bc0113ce7ae3da97cb900
Willy Seminario <wseminario@Willys-MacBook-Pro.local>
2026-03-10 14:31:55 -0500

    Revert "Fix ReservationRepository to properly fetch master_calendar events for companies"
    This reverts commit 083a953dd2f2e5a51f933dd6e7924977a353245d.

--- lib/features/calendar/data/reservation_repository.dart
+++ lib/features/calendar/data/reservation_repository.dart
@@ -14,9 +14,11 @@ class DailyReservations extends _$DailyReservations {
   Stream<List<Reservation>> build(DateTime date) async* {
     final activeCompanyId = ref.watch(authControllerProvider)?.activeCompanyId;
     
-    // The master_calendar is currently a global/root node populated externally (e.g. via Make.com).
-    // It is NOT stored inside the individual company buckets.
-    final ref_calendar = FirebaseDatabase.instance.ref('master_calendar');
+    final DatabaseReference dbRef = activeCompanyId == null 
+        ? FirebaseDatabase.instance.ref()
+        : FirebaseDatabase.instance.ref('companies/$activeCompanyId');
+
+    final ref_calendar = dbRef.child('master_calendar');
     final propRepo = PropertyRepository(activeCompanyId: activeCompanyId);
 
     await for (final event in ref_calendar.onValue) {
